d4ee69109b539e808aba5b86fc5298cb6616fbaa
Dathan <dathan@gmail.com>
2020-04-18 23:09:46 -0700

    Add Log4jTest and README instructions on how to configure Log4j during test run

--- src/test/java/com/jedaway/Log4jTest.java
+++ src/test/java/com/jedaway/Log4jTest.java
@@ -1,4 +1,17 @@
 package com.jedaway;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test that log4j is configured correctly.
+ */
 public class Log4jTest {
+    @Test
+    public void test() {
+//        System.setProperty("log4j.debug", "true");
+        Logger logger = LogManager.getLogger(Log4jTest.class);
+        logger.debug("hi");
+    }
 }
