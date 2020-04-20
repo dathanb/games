c5f25178406fa0f5a536a144f72bd2b7142bb8a8
Dathan <dathan@gmail.com>
2020-04-19 20:41:43 -0700

    Factor out OrderingPositionEvaluator#getBucketScore
    This was originally part of a feature to cache bucket scores. But the
    bit twiddling logic is apparenlty fast enough that the cache overhead
    slows it down by half. So I took out the cache. I even tried with a
    straight HashMap, and it slowed down the app by 20%. I wonder if it
    would be a net win if we used deeper buckets? But we can't handle
    deeper buckets yet, so we won't look that way yet.

--- src/main/java/com/jedaway/sorting/OrderingPositionEvaluator.java
+++ src/main/java/com/jedaway/sorting/OrderingPositionEvaluator.java
@@ -7,34 +7,41 @@ import com.jedaway.game.PositionEvaluator;
  * gives extra points to homogenous groups of colors on the bottom of the bucket).
  */
 public class OrderingPositionEvaluator implements PositionEvaluator<SortingGame, SortingGameMove> {
+    public OrderingPositionEvaluator() {
+    }
+
     @Override
     public double evaluate(SortingGame gameState) {
         double maxScore = (gameState.getBuckets().size() - 1) * Math.pow(2, gameState.getBuckets().get(0).capacity);
 
         double positionScore = gameState.getBuckets().stream()
+                // TODO: consider using parallel stream
                 .filter(bucket -> bucket.size() > 0)
                 .map(Bucket::getRawStack)
-                .reduce(0, (cumulativeScore, stack) -> {
-                    int bucketScore = 0;
-                    int groupScore = 1; // score for current group of consecutive homogeneous colors
-                    long previousColor = stack & 0x0f;
-                    while (stack != 0) {
-                        long currentColor = stack & 0x0f;
-                        stack >>>= 4;
-                        if (currentColor != previousColor) {
-                            bucketScore += groupScore;
-                            groupScore = 1;
-                            continue;
-                        }
-                        groupScore *= 2;
-                    }
-                    bucketScore += groupScore;
-                    return cumulativeScore + bucketScore;
-                }, Integer::sum);
+                .reduce(0.0, (cumulativeScore, stack) -> cumulativeScore + getBucketScore(stack), Double::sum);
 
         if (positionScore >= maxScore) {
             return Double.POSITIVE_INFINITY; // score for terminal position
         }
         return positionScore;
+    }
+
+    private double getBucketScore(final long stack) {
+        long tempStack = stack;
+        int bucketScore = 0;
+        int groupScore = 1; // score for current group of consecutive homogeneous colors
+        long previousColor = tempStack & 0x0f;
+        while (tempStack != 0) {
+            long currentColor = tempStack & 0x0f;
+            tempStack >>>= 4;
+            if (currentColor != previousColor) {
+                bucketScore += groupScore;
+                groupScore = 1;
+                continue;
+            }
+            groupScore *= 2;
+        }
+        bucketScore += groupScore;
+        return bucketScore;
     }
 }
